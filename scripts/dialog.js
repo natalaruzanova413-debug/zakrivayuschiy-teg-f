aacb7686541fbce1ae24abf08f9251a369f6578d
NATALI <mulga4eva.natali26@yandex.ru>
2026-02-17 12:34:00 +0400

    ревью, исправила ошибки

--- scripts/dialog.js
+++ scripts/dialog.js
@@ -1,4 +1,4 @@
-const saveButton = document.querySelector('.content__save-button');
+const saveButton = document.querySelector('.save-button');
 const dialog = document.querySelector('#saveDialog');
 
 saveButton.addEventListener('click', () => {
